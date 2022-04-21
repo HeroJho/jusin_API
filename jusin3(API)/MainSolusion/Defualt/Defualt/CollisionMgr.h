de1f7bedd0a2503d0b3de8264f516bbc53d2bd99
HeroJho <theherojho@naver.com>
2022-04-21 19:58:54 +0900

    충돌처리 숙제: 플레이어가 보는 방향에 따라 앞, 뒤 이동 구현

---
+++ jusin3(API)/MainSolusion/Defualt/Defualt/CollisionMgr.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CObj.h"
+
+class CollisionMgr
+{
+public:
+	CollisionMgr();
+	~CollisionMgr();
+
+public:
+	static bool		Check_Sphere(CObj* pDest, CObj* pSour);
+	static void		Collision_Rect(list<CObj*> _Dest, list<CObj*> _Sour);
+	static void		Collision_Sphere(list<CObj*> _Dest, list<CObj*> _Sour);
+};
+
